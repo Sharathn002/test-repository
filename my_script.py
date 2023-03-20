e22160a5d891bbf6a047d6b15d596c5c7fd8eb9a
Sharathn002 <125349872+Sharathn002@users.noreply.github.com>
2023-03-20 10:11:23 +0530

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,16 @@
+import sys
+
+# Accept command line arguments
+if len(sys.argv) > 1:
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    print(f'Argument 1: {arg1}')
+    print(f'Argument 2: {arg2}')
+else:
+    print('No arguments provided.')
+
+# Get system-specific parameters and configuration
+print(f'System platform: {sys.platform}')
+print(f'Python version: {sys.version}')
+print(f'Python executable path: {sys.executable}')
+print(f'Search path for modules: {sys.path}')
