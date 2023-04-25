4cc55b9dde5022ee4a59983cdba47e4532547f8e
Sharathn002 <125349872+Sharathn002@users.noreply.github.com>
2023-04-25 23:37:47 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,13 +10,7 @@ def main():
     parser.add_argument('--start_time', help='start time', default='0')
     parser.add_argument('--end_time', help='end time', default='0')
     args = parser.parse_args()
-
-    if args.function == 'session_id':
-        print(args.session_ID,args.connection_id)
-    elif args.function.lower() == 'date':
-        print(args.dates,args.connection_id)
-    elif args.function.lower() == 'time_range':
-        print(args.dates,args.start_time,args.end_time,args.connection_id)
+    print(args.dates,args.start_time,args.session_ID,args.end_time,args.connection_id)
     
 if __name__=='__main__':
     main()
